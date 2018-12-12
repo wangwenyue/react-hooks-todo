f403174e32f5d5b1a58c51fc9eed80fec1db87e9
Wenyue Wang <realwwy@cryptape.com>
2018-12-12 11:38:36 +0800

    add production version

---
+++ build/precache-manifest.c81408127db81787abb6bf59a1ce0596.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3e78817fbcbf5da096d8",
+    "url": "/static/js/main.3e78817f.chunk.js"
+  },
+  {
+    "revision": "60bada7cce12a3b87a9a",
+    "url": "/static/js/1.60bada7c.chunk.js"
+  },
+  {
+    "revision": "3e78817fbcbf5da096d8",
+    "url": "/static/css/main.84636e07.chunk.css"
+  },
+  {
+    "revision": "6079109c2b7927e162b09bfc71bcf28b",
+    "url": "/index.html"
+  }
+];
